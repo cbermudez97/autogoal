ba66954287cc9fa5dad74541e7e45c6437a344e7
Carlos Bermudez <cbermudez.dev@gmail.com>
2021-10-11 12:14:04 -0400

    Implement find distillers method

---
+++ autogoal/experimental/distillation/distillers/__init__.py
@@ -0,0 +1,46 @@
+def find_distillers(include=None, exclude=None, modules=None):
+    import inspect
+    import re
+
+    from .base_distiller import AlgorithmDistillerBase
+
+    result = []
+
+    if include:
+        include = f".*({include}).*"
+    else:
+        include = r".*"
+
+    if exclude:
+        exclude = f".*({exclude}).*"
+
+    if modules is None:
+        modules = []
+
+        try:
+            from autogoal.experimental.distillation.distillers import keras
+
+            modules.append(keras)
+        except ImportError as e:
+            pass
+
+    for module in modules:
+        for _, cls in inspect.getmembers(module, inspect.isclass):
+            if not issubclass(cls, AlgorithmDistillerBase):
+                continue
+
+            if not hasattr(cls, "can_distill") or not hasattr(cls, "distill"):
+                continue
+
+            if cls.__name__.startswith("_"):
+                continue
+
+            if not re.match(include, repr(cls)):
+                continue
+
+            if exclude is not None and re.match(exclude, repr(cls)):
+                continue
+
+            result.append(cls)
+
+    return result
