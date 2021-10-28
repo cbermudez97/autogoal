b801a9095cb3cb671fada6cb380f460ed28b4892
Carlos Bermudez <cbermudez.dev@gmail.com>
2021-10-28 14:52:10 -0400

    Adapt Bert embedding distiller to seq algorithms

--- autogoal/experimental/distillation/distillers/transformers/transformer_distiller.py
+++ autogoal/experimental/distillation/distillers/transformers/transformer_distiller.py
@@ -14,23 +14,42 @@ from .utils import (
 
 class BertEmbeddingDistiller(AlgorithmDistillerBase):
     def can_distill(self, algorithm: AlgorithmBase) -> bool:
-        return isinstance(algorithm, BertEmbedding)
+        # TODO: Add other use cases
+        return self._is_seq_alg(algorithm)
+
+    def _is_seq_alg(self, algorithm: AlgorithmBase) -> bool:
+        # TODO: Improve SeqAlgorithm detection
+        return "inner" in algorithm.__dict__ and isinstance(
+            algorithm.__dict__["inner"], BertEmbedding
+        )
+
+    def extract_model(self, algorithm: AlgorithmBase):
+        if self._is_seq_alg(algorithm):
+            return algorithm.inner.model
+        return None
+
+    def extract_tokenizer(self, algorithm: AlgorithmBase):
+        if self._is_seq_alg(algorithm):
+            return algorithm.inner.tokenizer
+        return None
 
     def distill(
         self,
-        algorithm: BertEmbedding,
+        algorithm: AlgorithmBase,
         train_inputs: dict,
         test_inputs: dict,
         registry: List = None,
     ) -> AlgorithmBase:
         if not self.can_distill(algorithm):
-            raise ValueError("Param 'algorithm' must be a BertEmbedding instance")
+            raise ValueError("Param 'algorithm' must use a BertEmbedding instance")
 
-        model = algorithm.model
-        distilled_model = algorithm.model
-        distilled_tokenizer = algorithm.tokenizer
+        alg_model = self.extract_model(algorithm)
+        alg_tokenizer = self.extract_tokenizer(algorithm)
+
+        distilled_model = alg_model
+        distilled_tokenizer = alg_tokenizer
         try:
-            model_name = model.config.name_or_path
+            model_name = alg_model.config.name_or_path
             distilled_model_name = get_pre_trained_name(model_name)
             if distilled_model_name:
                 distilled_model = get_pre_trained_model(distilled_model_name)
@@ -47,11 +66,18 @@ class BertEmbeddingDistiller(AlgorithmDistillerBase):
         return distilled_algorithm
 
     def build_distilled(
-        self, algorithm: BertEmbedding, new_model, new_tokenizer
-    ) -> BertEmbedding:
-        new_algorithm = BertEmbedding(
-            merge_mode=algorithm.merge_mode, verbose=algorithm.verbose
+        self, algorithm: AlgorithmBase, new_model, new_tokenizer
+    ) -> AlgorithmBase:
+        if self._is_seq_alg(algorithm):
+            return self._build_seq_alg(algorithm, new_model, new_tokenizer)
+        return algorithm
+
+    def _build_seq_alg(self, algorithm: AlgorithmBase, new_model, new_tokenizer):
+        new_inner = BertEmbedding(
+            merge_mode=algorithm.inner.merge_mode, verbose=algorithm.inner.verbose
         )
-        new_algorithm.model = new_model
-        new_algorithm.tokenizer = new_tokenizer
+        new_inner.model = new_model
+        new_inner.tokenizer = new_tokenizer
+        new_algorithm = algorithm.__class__()
+        new_algorithm.inner = new_inner
         return new_algorithm
