a2d0b1878a78a7afdc3035613abd43c4c1b742b4
Carlos Bermudez <cbermudez.dev@gmail.com>
2021-11-15 14:14:14 -0500

    Fix error

--- autogoal/experimental/distillation/distillers/keras/model_distillers/basic_no_distiller.py
+++ autogoal/experimental/distillation/distillers/keras/model_distillers/basic_no_distiller.py
@@ -2,8 +2,8 @@ from .distiller_base import DistillerBase
 
 
 class BasicClassifierNoDistiller(DistillerBase):
-    def compile(self, optimizer, metrics, task_loss_fn, alpha=0, **kwargs):
-        super().compile(optimizer, metrics, task_loss_fn, alpha=alpha, **kwargs)
+    def compile(self, optimizer, metrics, task_loss_fn, **kwargs):
+        super().compile(optimizer, metrics, task_loss_fn, **kwargs)
 
     def calculate_distillation_loss(self, x, y):
         return 0
